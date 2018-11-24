3c019caaa4c3ac691c8c96b4c3cf6747605e1607
Kristiyan Dzhakov <kristiyandzhakov@kristiyans-mbp.home>
2018-11-24 00:49:57 +0000

    Implemented more routes

--- App.js
+++ App.js
@@ -4,17 +4,24 @@ import { createAppContainer, createStackNavigator } from 'react-navigation';
 // Pages
 import Home from './pages/HomePage.js';
 import AboutMe from './pages/AboutPage.js';
+import BookingPage from './pages/BookingPage.js';
+import AvailabilityPage from './pages/AvailabilityPage.js';
+import FaqPage from './pages/FaqPage.js';
+import UsefulLinksPage from './pages/UsefulLinksPage.js';
+
 
 
 
 const AppScreens = createStackNavigator({
   Home: Home,
-  About: AboutMe
+  About: AboutMe,
+  Book: BookingPage,
+  Availability: AvailabilityPage,
+  Faq: FaqPage,
+  Links: UsefulLinksPage
 });
 
 const App = createAppContainer(AppScreens);
-
-
 
 export default App;
 
