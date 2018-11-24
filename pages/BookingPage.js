20c1efbeb177ff3f18b06225685a749197f8f8a8
Kristiyan Dzhakov <kristiyandzhakov@kristiyans-mbp.home>
2018-11-24 01:34:20 +0000

    Implemented text inputs + a little bit of styling

--- pages/BookingPage.js
+++ pages/BookingPage.js
@@ -1,29 +1,87 @@
-import React , { Component } from 'react';
-import { Platform, StyleSheet, Text, View, Button, TouchableHighlight } from 'react-native';
+import React ,{ Component } from 'react';
+import { Platform, StyleSheet, Text, View, Button, TouchableHighlight, Dimensions } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
+import {TextInput } from 'react-native-paper';
 
 class BookingPage extends Component {
+    state =  {
+        firstName: '',
+        lastName: '',
+        phoneNumber: '',
+        email: '',
+        address: ''
+    }
     static navigationOptions = {
         title: 'Book'
     }
     render(){
         return(
             <LinearGradient colors={['#4c669f', '#3b5998', '#192f6a']} style={styles.container}>
-                <Text>This is the booking page</Text>
+                <View style={styles.innerView}>
+                    <Text>This is the booking page</Text>
+                    <TextInput
+                        style={styles.input}
+                        label='First Name'
+                        mode="flat"
+                        placeholder="Enter first name..."
+                        value={this.state.text}
+                        onChangeText={firstName => this.setState({ firstName })}
+                    />
+                    <TextInput
+                        style={styles.input}
+                        label='Last Name'
+                        mode="flat"
+                        placeholder="Enter last name..."
+                        value={this.state.text}
+                        onChangeText={lastName => this.setState({ lastName })}
+                    />
+                    <TextInput
+                        style={styles.input}
+                        label='Phone number'
+                        mode="flat"
+                        placeholder="Enter phone number..."
+                        value={this.state.text}
+                        onChangeText={phoneNumber => this.setState({ phoneNumber })}
+                    />
+                    <TextInput
+                        style={styles.input}
+                        label='Email'
+                        mode="flat"
+                        placeholder="Enter email..."
+                        value={this.state.text}
+                        onChangeText={email => this.setState({ email })}
+                    />
+                    <TextInput
+                        style={styles.input}
+                        label='Address'
+                        mode="flat"
+                        placeholder="Enter address..."
+                        value={this.state.text}
+                        onChangeText={address => this.setState({ address })}
+                    />
+                </View>
+                
             </LinearGradient>
         );
     };
 };
 
+let width = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
+      alignItems: 'center'
     },
+    innerView: {
+        flex: 1,
+        backgroundColor: 'transparent'
+    },
+    input: {
+        backgroundColor: 'transparent',
+        width: width * .8,
+        marginTop: 20
+    }
   });
 
   export default BookingPage;
-
-  // not yet routed
