fe79beb81317124e2d6d295a1e0d6e35a5f97860
Kristiyan Dzhakov <kristiyandzhakov@kristiyans-mbp.home>
2018-11-25 21:09:11 +0000

    Small changes made on the icons

--- icons/AboutPageIcons.js
+++ icons/AboutPageIcons.js
@@ -6,7 +6,8 @@ import BookingIcon from 'react-native-vector-icons/FontAwesome';
 import FeatherIcons from 'react-native-vector-icons/Feather';
 
 
-export const bookIcon = (<BookingIcon name="pencil-square-o" size={40} color="purple" />);
-export const calendarIcon = (<SocialIcon name="calendar" size={40} color="purple" />);
-export const questionIcon = (<SocialIcon name="questioncircleo" size={40} color="purple" />);
-export const externalLinkIcon = (<FeatherIcons name="external-link" size={40} color="purple" />);
+
+export const bookIcon = (<BookingIcon name="pencil-square-o" size={40} style={{marginRight:20}} color="purple" />);
+export const calendarIcon = (<SocialIcon name="calendar" size={40} style={{marginRight:20}} color="purple" />);
+export const questionIcon = (<SocialIcon name="questioncircleo" size={40} style={{marginRight:20}} color="purple" />);
+export const externalLinkIcon = (<FeatherIcons name="external-link" size={40} style={{marginRight:20}} color="purple" />);
