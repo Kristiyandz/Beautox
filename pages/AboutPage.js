8ca83f4374e9e2ed1421c96404e162b4fada56ce
Kristiyan Dzhakov <kristiyandzhakov@kristiyans-mbp.home>
2018-11-25 01:05:14 +0000

    Updated styles and page layout

--- pages/AboutPage.js
+++ pages/AboutPage.js
@@ -1,43 +1,49 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, TouchableHighlight, FlatList} from 'react-native';
 import { bookIcon, calendarIcon, questionIcon, externalLinkIcon } from '../icons/AboutPageIcons.js';
+import LinearGradient from 'react-native-linear-gradient';
+
 
 
 class AboutMe extends Component {
     static navigationOptions = {
       title: "About Me",
+      headerStyle: { backgroundColor: '#712084'}
     }
     render() {
       return (
-        <View style={styles.container}>
-          <View style={styles.topBox}>
-          <View style={styles.circleOuter} />
-            <View style={styles.circle} />
-            
-          </View>
-          <View style={styles.middleBox}>
+        <LinearGradient colors={['#712084', '#4050B2', '#0073CC', '#0091D3', '#00ABC9']} style={styles.container}>
+          <View style={styles.container}>
+            <View style={styles.topBox}>
+            <View style={styles.circleOuter} />
+              <View style={styles.circle} />
+              
+            </View>
+            <View style={styles.middleBox}>
 
+            </View>
+            <View style={styles.bottomBox}>
+              <FlatList
+                contentContainerStyle={styles.listItem}
+                data={[
+                  {key: 'Book a free apointment', page:'Book', icon: bookIcon},
+                  {key: 'Availability', page:'Availability', icon:calendarIcon},
+                  {key: 'FAQ', page:'Faq', icon: questionIcon},
+                  {key: 'Useful Links', page: 'Links', icon: externalLinkIcon},
+                ]}
+                onPress={() => this.props.navigation.navigate('Book')}
+                renderItem={({item}) => {
+                    return (
+                      <TouchableHighlight onPress={() => this.props.navigation.navigate(`${item.page}`)}>
+                        <Text >{item.icon}{item.key}</Text>
+                      </TouchableHighlight>
+                    )
+                }}
+              />
+            </View>
           </View>
-          <View style={styles.bottomBox}>
-            <FlatList
-              contentContainerStyle={styles.listItem}
-              data={[
-                {key: 'Book a free apointment', page:'Book', icon: bookIcon},
-                {key: 'Availability', page:'Availability', icon:calendarIcon},
-                {key: 'FAQ', page:'Faq', icon: questionIcon},
-                {key: 'Useful Links', page: 'Links', icon: externalLinkIcon},
-              ]}
-              onPress={() => this.props.navigation.navigate('Book')}
-              renderItem={({item}) => {
-                  return (
-                    <TouchableHighlight onPress={() => this.props.navigation.navigate(`${item.page}`)}>
-                      <Text >{item.icon}{item.key}</Text>
-                    </TouchableHighlight>
-                  )
-              }}
-            />
-          </View>
-        </View>
+        </LinearGradient>
+
       );
     };
   };
@@ -48,32 +54,32 @@ class AboutMe extends Component {
     },
     topBox: {
       flex:3,
-      backgroundColor: 'red',
+      backgroundColor: 'transparent',
       justifyContent:'center',
       alignItems: 'center'
     },
     middleBox: {
       flex:1,
-      backgroundColor: 'yellow'
+      backgroundColor: 'transparent'
     },
     bottomBox: {
       flex:4,
-      backgroundColor: 'orange',
+      backgroundColor: 'transparent',
       padding: 20
       
     },
     circle: {
-      width: 160,
-      height: 160,
-      borderRadius: 160/2,
+      width: 190,
+      height: 190,
+      borderRadius: 190/2,
       backgroundColor: 'green',
       position: 'absolute'
   },
     circleOuter: {
-      width: 180,
-      height: 180,
-      borderRadius: 180/2,
-      backgroundColor: 'black'
+      width: 220,
+      height: 220,
+      borderRadius: 220/2,
+      backgroundColor: '#712084'
   },
     listItem: {
       flex:1,
