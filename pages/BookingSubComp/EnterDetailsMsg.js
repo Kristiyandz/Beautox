90627a978b5064d72e50c69421b75470e62aff3b
Kristiyan Dzhakov <kristiyandzhakov@Kristiyans-MacBook-Pro.local>
2018-12-23 00:44:59 +0000

    added few styles

--- pages/BookingSubComp/EnterDetailsMsg.js
+++ pages/BookingSubComp/EnterDetailsMsg.js
@@ -4,7 +4,7 @@ import { Text, View, StyleSheet } from 'react-native';
 const EnterDetailsMsg = () => {
     const { detailMsg } = styles;
     return (
-        <View>
+        <View style={styles.messageContainer}>
             <Text style={detailMsg}>To book a home visit, please enter your{'\n'}details bellow.</Text>
             <Text style={detailMsg}>We will contact you as soon as possible.</Text>
         </View>
@@ -12,8 +12,12 @@ const EnterDetailsMsg = () => {
 };
 
 const styles = StyleSheet.create({
+    messageContainer: {
+        marginTop: 20
+    },
     detailMsg: {
         color: 'whitesmoke',
+        padding: 10,
         // paddingTop: 40
     }
 });
